86d687e31f72ff5260876ad02e81b7f88269ce2d
Reyabdul <reyabdul@gmail.com>
2025-01-06 16:56:30 +0900

    Add event listners to add new names to phonebook

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,45 @@ import { useState } from "react";
 const App = () => {
 
   const [persons, setPersons] = useState([
-    { name: 'Artos Hellas' }
+    { 
+      name: 'Artos Hellas',
+      id: 1,
+     }
   ])
+
+  // console.log(persons)
   
-  const [newName, setNewName] =useState("")
+  const [newName, setNewName] = useState("")
+
+  const addName = (event) => {
+    event.preventDefault()
+    // console.log("button clicked", event.target)
+    const peronObject = {
+      name: newName,
+      id: String(persons.length + 1) 
+    }
+
+    setPersons(persons.concat(peronObject))
+    setNewName(" ")
+  }
+
+  console.log(persons)
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addName}>
         <div>
-          name: <input />
+          name: 
+          <input 
+            value={newName}
+            onChange={handleNameChange}  
+          />
         </div>
         <div>
           <button type="submit">
@@ -23,6 +51,13 @@ const App = () => {
       </form>
       
       <h2>Numbers</h2>
+      <ul>
+        {persons.map(person =>
+          <li key={person.id}>{person.name}</li>
+        )}
+      </ul>
+      <div>debug: {newName}</div>
+
     </div>
   )
 }
