11a943e48dcb1a1f714b0d9d550d3b85e892f034
Reyabdul <reyabdul@gmail.com>
2025-01-07 15:08:24 +0900

    Add function that check if name exists

--- src/App.jsx
+++ src/App.jsx
@@ -1,65 +1,68 @@
 import { useState } from "react";
 
 const App = () => {
-
   const [persons, setPersons] = useState([
-    { 
-      name: 'Artos Hellas',
-      id: 1,
-     }
-  ])
+    {
+      name: "Artos Hellas",
+      id: "1",
+    },
+  ]);
 
   // console.log(persons)
-  
-  const [newName, setNewName] = useState("")
+
+  const [newName, setNewName] = useState("");
 
   const addName = (event) => {
-    event.preventDefault()
+    event.preventDefault();
     // console.log("button clicked", event.target)
-    const peronObject = {
+
+    const newContact = {
       name: newName,
-      id: String(persons.length + 1) 
+      id: String(persons.length + 1),
+    };
+
+    const existingNames = persons.map((person) => {
+      return person.name;
+    });
+
+    if (existingNames.includes(newContact.name)) {
+      alert(`${newName} already exists`);
+      return false;
     }
 
-    setPersons(persons.concat(peronObject))
-    setNewName(" ")
-  }
+    setPersons(persons.concat(newContact));
+    setNewName("");
+  };
 
-  console.log(persons)
+  // console.log(persons)
 
   const handleNameChange = (event) => {
-    console.log(event.target.value)
-    setNewName(event.target.value)
-  }
+    // console.log(event.target.value)
+    setNewName(event.target.value);
+  };
 
   return (
     <div>
-      <h2>Phonebook</h2>
+      <h2>Phone book</h2>
       <form onSubmit={addName}>
         <div>
-          name: 
-          <input 
-            value={newName}
-            onChange={handleNameChange}  
-          />
+          name:
+          <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
-          <button type="submit">
-            Add
-          </button>
+          <button type="submit">Add</button>
         </div>
       </form>
-      
+
       <h2>Numbers</h2>
       <ul>
-        {persons.map(person =>
+        {persons.map((person) => (
           <li key={person.id}>{person.name}</li>
-        )}
+        ))}
       </ul>
       <div>debug: {newName}</div>
-
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
